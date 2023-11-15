8c67f9590c39561bc510bd781bcad69251c9d807
Zasics <zeyad.a.arafa@gmail.com>
2023-11-15 11:16:28 +0200

    registering pharmacists can now upload documents

--- backend/controllers/pharmacistController.js
+++ backend/controllers/pharmacistController.js
@@ -5,6 +5,7 @@ require('dotenv').config()
  const jwt= require('jsonwebtoken')
  const bcrypt = require('bcrypt')
  const validator= require('validator')
+ const Document= require('../models/documentModel')
 
 
 
@@ -147,6 +148,31 @@ const updatePharmacistPassword = async (req, res) => {
     res.status(200).json(pharmacist);
 };
 
+const uploadDocument = async (req, res) => {
+    if(!req.file){
+        return res.status(400).json({error: 'No file was uploaded'})
+    }
+
+    const document = req.file.filename
+
+    if (req.pharmacist) {
+        // If a pharmacist is logged in
+        const pharmacistId = req.pharmacist._id;
+        const newDocument= new Document({
+            pharmacistId,
+            document
+        })
+
+        try{
+            await newDocument.save()
+            res.status(201).json(newDocument)
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error: error.message})
+        }
+    }
+}
+
 module.exports= {
     getApprovalPharmacists,
     getPharmacists,
@@ -155,5 +181,6 @@ module.exports= {
     updatePharmacist,
     updatePharmacistPassword,
     signupPharmacist,
-    loginPharmacist
+    loginPharmacist,
+    uploadDocument
 }
--- frontend/src/pages/Pharmacist/NonApprovedPharmacist.js
+++ frontend/src/pages/Pharmacist/NonApprovedPharmacist.js
@@ -1,9 +1,78 @@
 
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../hooks/useAuthContext';
+//import { useLocation } from 'react-router-dom';
+import axios from 'axios'
+
 const NonApprovedPharmacist = () => {
+
+  const { user } = useAuthContext()
+  const [error, setError] = useState(null)
+  const [done, setDone] = useState(null)
+  const [file, setFile] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
+
+  //const location = useLocation();
+  //const { appointment } = location.state;
+
+  //const { pharmacistId } = appointment;
+
+  const upload = () => {
+    setError(null);
+    setDone(false);
+    setEmptyFields([]);
+
+    if (!user) {
+      setError('You must be logged in')
+      return
+    }
+    console.log(user.id)
+
+    if (!file) {
+      setError('You must choose a file')
+      setEmptyFields(['file']);
+      return
+    }
+    
+    const formData = new FormData()
+    formData.append('document', file)
+    axios.post(`/api/pharmacist/document`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${user.token}`
+      }
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      setDone(false)
+      setError(err.message)
+      return
+    })
+    setDone(true)
+  }
 
     return(
         <div>
-            Your Request Has been submitted. Please Await Admin approval.
+            <center><h3>
+            Your Request Has been submitted. Please upload required documents and await Admin approval.<br></br>
+            </h3>
+            (such as ID, pharmacy degree and Working licenses)
+            </center>
+            <label>Document File: </label>
+            <input
+                type="file"
+                onChange={(e) => setFile(e.target.files[0])}
+                className={emptyFields.includes('file') ? 'error' : ''}
+            />
+            <button type='button' onClick={upload}>Add Document</button>
+            {done && <center><h4>Document Uploaded Successfully!</h4></center>}
+            {error && <div className="error">{error}</div>}
+            <div className='back-button'>
+                <Link to="/">
+                <button className='normal-button'>Back To Home</button>
+                </Link>
+            </div>
         </div>
     )
 }
